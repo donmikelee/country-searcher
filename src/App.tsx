97aac9add85ae6375b3a6a69ec1c2255c6eb9b4e
Michał Górny <umpex94@gmail.com>
2023-12-21 17:46:37 +0100

    Building navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,12 @@
-import Header from "./components/sections/Header";
 
-interface BEMProps {
-  block: string;
-  element?: string;
-  modifiers?: string[];
-}
-
-const generateBEMClassName = ({ block, element, modifiers }: BEMProps): string => {
-  let className = block;
-
-  if (element) {
-    className += `__${element}`;
-  }
-
-  if (modifiers && modifiers.length > 0) {
-    className += modifiers.map((modifier) => `${block}--${modifier}`).join('');
-  }
-
-  return className
-}
+import Nav from "./components/sections/Nav/Nav";
+import "./style/style.css";
 
 const App = () => {
 
-  const appClass = generateBEMClassName( { block: 'csApp' });
-  const headerClass = generateBEMClassName( { block: 'csApp', element: 'Header' });
-
   return ( 
-    <div className={appClass}>
-      <Header className={headerClass}/>
+    <div className="App">
+      <Nav/>
     </div>
   );
 }
---
+++ src/components/sections/Nav/Nav.tsx
@@ -0,0 +1,12 @@
+const Nav = () => {
+    return (  
+        <nav className="navigation">
+            <div className="logo">Where in the world?</div>
+            <button className="theme-changer">
+                <span className="theme-changer-text">Dark Mode</span>
+            </button>
+        </nav>
+    );
+}
+ 
+export default Nav;
