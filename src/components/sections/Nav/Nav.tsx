f0a026e21e1e0f3d54318c4c54e21595e0fa23ec
Michał Górny <umpex94@gmail.com>
2024-01-03 18:27:02 +0100

    Close logo in a tag, add reseting styles
    Close logo in a tag, add reseting styles

--- src/components/sections/Nav/Nav.tsx
+++ src/components/sections/Nav/Nav.tsx
@@ -1,7 +1,9 @@
 const Nav = () => {
     return (  
         <nav className="navigation">
-            <div className="logo">Where in the world?</div>
+            <div className="logo">
+                <a href="http://localhost:5173/" className="logo-link">Where in the world?</a>
+            </div>
             <button className="theme-changer">
                 <span className="theme-changer-text">Dark Mode</span>
             </button>
