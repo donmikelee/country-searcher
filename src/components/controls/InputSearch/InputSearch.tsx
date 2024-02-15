2d6b03c29036cefe8c14f8ca9b4ca976e4c45eba
Michał Górny <umpex94@gmail.com>
2024-02-15 11:42:05 +0100

    Dynamic render of countries using context api

--- src/components/controls/InputSearch/InputSearch.tsx
+++ src/components/controls/InputSearch/InputSearch.tsx
@@ -1,8 +1,17 @@
-const InputSearch = () => {
+import { useData } from "../../../data/DataContext";
+
+const InputSearch = () => {   
+
+    const { setTypedCountryName } = useData();
+
+    const handleChangeCountryName = (e: any) => {
+        setTypedCountryName(e.target.value);
+    }
+
     return (
         <div className="input-container">
             <label htmlFor="input-search" style={{display: 'none'}}>Search for a country</label>
-            <input type="search" id="input-search" placeholder="Search for a country..." className="input-search" ></input>
+            <input type="search" id="input-search" placeholder="Search for a country..." className="input-search" onChange={handleChangeCountryName}></input>
         </div>  
     );
 }
